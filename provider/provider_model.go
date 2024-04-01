37b81d39b077631dae94e9177cc312f301b7cde1
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2024-04-01 21:29:51 +0800

    Commit message seems not to match the diff content. Here is the correct commit message based on provided diff:
    Fixed newline error at end of file
    
    An unnecessary linting error, due to missing newline at the end of file in provider_model.go, has been corrected. This follows the widely accepted practice that a file should end with an empty newline.

--- provider/provider_model.go
+++ provider/provider_model.go
@@ -1,1 +1,27 @@
-package providerimport (	"github.com/hashicorp/terraform-plugin-framework/types"	"github.com/yunarta/terraform-atlassian-api-client/bamboo")type EndPoint struct {	EndPoint types.String `tfsdk:"endpoint"`	Token    types.String `tfsdk:"token"`}type BambooRss struct {	Server   types.String `tfsdk:"server"`	Name     types.String `tfsdk:"name"`	CloneUrl types.String `tfsdk:"clone_url"`}type BambooProviderConfig struct {	Bamboo    EndPoint  `tfsdk:"bamboo"`	BambooRss BambooRss `tfsdk:"bamboo_rss"`}type BambooProviderData struct {	config BambooProviderConfig	client *bamboo.Client}
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/yunarta/terraform-atlassian-api-client/bamboo"
+)
+
+type EndPoint struct {
+	EndPoint types.String `tfsdk:"endpoint"`
+	Token    types.String `tfsdk:"token"`
+}
+
+type BambooRss struct {
+	Server   types.String `tfsdk:"server"`
+	Name     types.String `tfsdk:"name"`
+	CloneUrl types.String `tfsdk:"clone_url"`
+}
+
+type BambooProviderConfig struct {
+	Bamboo    EndPoint  `tfsdk:"bamboo"`
+	BambooRss BambooRss `tfsdk:"bamboo_rss"`
+}
+
+type BambooProviderData struct {
+	config BambooProviderConfig
+	client *bamboo.Client
+}
