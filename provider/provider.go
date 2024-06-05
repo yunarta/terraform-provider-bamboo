f2054e8f4ff3ee4cecb33e724849a2b58a031b78
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2024-06-05 11:06:28 -0400

    Add new Plan Resource to Bamboo Provider
    A new resource, Plan Resource, has been added to the Bamboo Provider in terraform-provider-bamboo. Now, it can be used for more efficient management and interaction with the Bamboo CI/CD platform.

--- provider/provider.go
+++ provider/provider.go
@@ -109,6 +109,7 @@ func (p *BambooProvider) DataSources(ctx context.Context) []func() datasource.Da
 
 func (p *BambooProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
+		NewPlanResource,
 		NewAgentAssignmentResource,
 		NewProjectResource,
 		NewProjectVariableResource,
