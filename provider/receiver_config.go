38b4f0f78fea25987751708e109eb380a56dc9dc
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2025-02-05 23:28:10 +0800

    Add ConfigureEphemeral function to support ephemeral configs
    Introduce a new `ConfigureEphemeral` function to handle ephemeral configuration requests. This ensures proper configuration of the receiver and adds error handling for unexpected provider data types. Relevant imports were also updated to support this functionality.

--- provider/receiver_config.go
+++ provider/receiver_config.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/yunarta/terraform-atlassian-api-client/bamboo"
 )
@@ -30,6 +31,23 @@ func ConfigureDataSource(receiver ConfigurableReceiver, ctx context.Context, req
 }
 
 func ConfigureResource(receiver ConfigurableReceiver, ctx context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
+	if request.ProviderData == nil {
+		return
+	}
+
+	data, ok := request.ProviderData.(*BambooProviderData)
+	if !ok {
+		response.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *AtlassianCloudProviderModel, got: %T. Please report this issue to the provider developers.", request.ProviderData),
+		)
+		return
+	}
+
+	receiver.setConfig(data.config, data.client)
+}
+
+func ConfigureEphemeral(receiver ConfigurableReceiver, ctx context.Context, request ephemeral.ConfigureRequest, response *ephemeral.ConfigureResponse) {
 	if request.ProviderData == nil {
 		return
 	}
